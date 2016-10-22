a90a6fe80305e9ebd6eb9ec1e84d7be364a91f9d
ahw <andrewhallagan@gmail.com>
2016-10-22 13:38:31 -0400

    ESLint
    Command line usage:
    
        ./node_modules/.bin/eslint index.js

--- index.js
+++ index.js
@@ -1,175 +1,191 @@
-'use strict'
+/* eslint-disable no-console */
+let argv = require('yargs');
+const s3 = require('s3');
+const fs = require('fs');
+const path = require('path');
+const async = require('async');
+const moment = require('moment');
+const ExifImage = require('exif').ExifImage;
+const exifDateParser = require('exif-date');
 
-let s3 = require('s3')
-let fs = require('fs')
-let path = require('path')
-let async = require('async')
-let moment = require('moment')
-let ExifImage = require('exif').ExifImage
-let exifDateParser = require('exif-date')
+const accessKeyId = fs.readFileSync(`${__dirname}/access_key_id`).toString().trim();
+const secretAccessKey = fs.readFileSync(`${__dirname}/secret_access_key`).toString().trim();
+const BUCKET_NAME = 'photos.andrewhallagan';
+const UPLOAD_LIST = 'upload-list.txt';
+const t0 = Date.now();
 
-let accessKeyId = fs.readFileSync(`${__dirname}/access_key_id`).toString().trim()
-let secretAccessKey = fs.readFileSync(`${__dirname}/secret_access_key`).toString().trim()
-const BUCKET_NAME = 'photos.andrewhallagan'
-const UPLOAD_LIST = 'upload-list.txt'
-let t0 = Date.now()
-
-let MAC_PHOTOS_LIBRARY_FILTER_FUNCTION = function(path) {
-    return /Masters/.test(path)
-        && /jpg/i.test(path)
+/**
+ *
+ * @param imagePath
+ * @returns {boolean}
+ * @constructor
+ */
+function MAC_PHOTOS_LIBRARY_FILTER_FUNCTION(imagePath) {
+    return /Masters/.test(imagePath)
+        && /jpg/i.test(imagePath)
         // && /2014\/(02|10)/.test(path)
-        && /2014\/03/.test(path)
+        && /2014\/03/.test(imagePath);
 }
 
-let WD_PASSPORT_FILTER_FUNCTION = function(path) {
-    return /png/i.test(path) || /jpg/i.test(path)
+/**
+ *
+ * @param imagePath
+ * @returns {boolean}
+ * @constructor
+ */
+function WD_PASSPORT_FILTER_FUNCTION(imagePath) { // eslint-disable-line no-unused-vars
+    return /png/i.test(imagePath) || /jpg/i.test(imagePath);
 }
 
 
-let SDMINI_FILTER_FUNCTION = function(path) {
-    return /jpg/i.test(path)
-        && !/Contact Sheet/.test(path)
-        && !/upholstery/.test(path)
-        && !/foam2/.test(path)
-        && !/home\/music/.test(path)
-        && !/Capture_/.test(path)
-        && !/markers/.test(path)
-        && !/timelapse_clips/.test(path)
-        && !/william_resize/.test(path)
-        && !/frame2_resize/.test(path)
-        && !/Downloads/.test(path)
-        && !/home\/2010/.test(path)
-        && !/home\/2011/.test(path)
-        && !/home\/55/.test(path)
-        && !/windows_desktop\/finishing/.test(path)
-        && !/windows_desktop\/foam1/.test(path)
-        && !/windows_desktop\/foam2/.test(path)
-        && !/windows_desktop\/foam3/.test(path)
-        && !/web_thumbnails_100/.test(path)
-        && !/web_thumbnails_75/.test(path)
-        && !/800x600/.test(path)
-        && !/resized/.test(path)
-        && !/resize/.test(path)
+/**
+ *
+ * @param imagePath
+ * @returns {boolean}
+ * @constructor
+ */
+function SDMINI_FILTER_FUNCTION(imagePath) { // eslint-disable-line no-unused-vars
+    return /jpg/i.test(imagePath)
+        && !/Contact Sheet/.test(imagePath)
+        && !/upholstery/.test(imagePath)
+        && !/foam2/.test(imagePath)
+        && !/home\/music/.test(imagePath)
+        && !/Capture_/.test(imagePath)
+        && !/markers/.test(imagePath)
+        && !/timelapse_clips/.test(imagePath)
+        && !/william_resize/.test(imagePath)
+        && !/frame2_resize/.test(imagePath)
+        && !/Downloads/.test(imagePath)
+        && !/home\/2010/.test(imagePath)
+        && !/home\/2011/.test(imagePath)
+        && !/home\/55/.test(imagePath)
+        && !/windows_desktop\/finishing/.test(imagePath)
+        && !/windows_desktop\/foam1/.test(imagePath)
+        && !/windows_desktop\/foam2/.test(imagePath)
+        && !/windows_desktop\/foam3/.test(imagePath)
+        && !/web_thumbnails_100/.test(imagePath)
+        && !/web_thumbnails_75/.test(imagePath)
+        && !/800x600/.test(imagePath)
+        && !/resized/.test(imagePath)
+        && !/resize/.test(imagePath);
 }
 
-let client = s3.createClient({
-  maxAsyncS3: 20,     // this is the default
-  s3RetryCount: 3,    // this is the default
-  s3RetryDelay: 1000, // this is the default
-  multipartUploadThreshold: 20971520, // this is the default (20 MB)
-  multipartUploadSize: 15728640, // this is the default (15 MB)
-  s3Options: {
-    accessKeyId,
-    secretAccessKey,
-    region: 'us-east-1',
-    // endpoint: 's3.yourdomain.com',
-    // sslEnabled: false
-    // any other options are passed to new AWS.S3()
-    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
-  },
-})
+const client = s3.createClient({
+    maxAsyncS3: 20, // this is the default
+    s3RetryCount: 3, // this is the default
+    s3RetryDelay: 1000, // this is the default
+    multipartUploadThreshold: 20971520, // this is the default (20 MB)
+    multipartUploadSize: 15728640, // this is the default (15 MB)
+    s3Options: {
+        accessKeyId,
+        secretAccessKey,
+        region: 'us-east-1',
+        // endpoint: 's3.yourdomain.com',
+        // sslEnabled: false
+        // any other options are passed to new AWS.S3()
+        // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
+    },
+});
 
 
-function walk(dir, filterFn) {
-    let results = []
-    filterFn = filterFn || (() => false)
-    let contents = fs.readdirSync(dir)
-    contents.map(file => path.join(dir, file)).forEach(path => {
-        let stats = fs.statSync(path)
-        if (stats.isFile() && filterFn(path)) {
-            console.log(`Found file ${path}`)
-            results.push(path)
+function walk(dir, filterFn = () => false) {
+    let results = [];
+    const contents = fs.readdirSync(dir);
+    contents.map(file => path.join(dir, file)).forEach((imagePath) => {
+        const stats = fs.statSync(imagePath);
+        if (stats.isFile() && filterFn(imagePath)) {
+            console.log(`Found file ${imagePath}`);
+            results.push(imagePath);
         } else if (stats.isDirectory()) {
-            results = results.concat(walk(path, filterFn))
+            results = results.concat(walk(imagePath, filterFn));
         }
-    })
+    });
 
-    return results
+    return results;
 }
 
 function getPreviouslyUploadedIndex() {
-    let index = {}
+    const index = {};
     try {
-        console.log('Reading file ' + path.resolve(UPLOAD_LIST))
-        let lines = fs.readFileSync(path.resolve(UPLOAD_LIST)).toString().split('\n')
-        lines.forEach(path => index[path.trim()] = 1)
-    } catch (e) {}
+        console.log(`Reading file ${path.resolve(UPLOAD_LIST)}`);
+        const lines = fs.readFileSync(path.resolve(UPLOAD_LIST)).toString().split('\n');
+        lines.forEach(imagePath => index[imagePath.trim()] = 1); // eslint-disable-line no-return-assign, max-len
+    } catch (e) {} // eslint-disable-line no-empty
 
-    return index
+    return index;
 }
 
-let s3startTime = Date.now()
-let previousUploadIndex = getPreviouslyUploadedIndex()
-let numIgnored = 0
+const s3startTime = Date.now();
+const previousUploadIndex = getPreviouslyUploadedIndex();
+let numIgnored = 0;
 
 function uploadToS3(image, index, total, callback) {
-    let parsedPath = path.parse(image)
-    let resolvedPath = path.resolve(image)
+    const parsedPath = path.parse(image);
+    const resolvedPath = path.resolve(image);
 
     if (previousUploadIndex[resolvedPath]) {
-        console.log(`[${index+1}/${total} Ignoring image ${resolvedPath} because it has already been uploaded`)
-        ++numIgnored
-        return callback()
+        console.log(`[${index + 1}/${total} Ignoring image ${resolvedPath} because it has already been uploaded`);
+        numIgnored += 1;
+        return callback();
     }
 
-    new ExifImage({ image }, (error, data) => {
-        let Key = undefined
+    new ExifImage({ image }, (error, data) => { // eslint-disable-line no-new
+        let Key;
 
         if (error && /No Exif segment found in the given image/i.test(error.message)) {
-            Key = 'UNKNOWN_DATE/' + parsedPath.base
+            Key = `UNKNOWN_DATE/${parsedPath.base}`;
         } else if (error) {
-            console.log(`Ignoring image ${image}. ${error.message}`)
+            console.log(`Ignoring image ${image}. ${error.message}`);
             // console.error(error)
-            return callback()
+            return callback();
         } else {
             // No error
-            let datetimeOriginal = data.image.DateTimeOriginal
-            console.log(`image.DateTime = ${data.image.DateTime}`)
-            console.log(`image.CreateDate = ${data.image.CreateDate}`)
-            console.log(`image.DateTimeOriginal = ${data.image.DateTimeOriginal}`)
-            console.log(`image.DateTimeDigitized = ${data.image.DateTimeDigitized}`)
+            const datetimeOriginal = data.image.DateTimeOriginal;
+            console.log(`image.DateTime = ${data.image.DateTime}`);
+            console.log(`image.CreateDate = ${data.image.CreateDate}`);
+            console.log(`image.DateTimeOriginal = ${data.image.DateTimeOriginal}`);
+            console.log(`image.DateTimeDigitized = ${data.image.DateTimeDigitized}`);
 
             if (datetimeOriginal) {
-                let timestamp = moment(exifDateParser.parse(data.image.DateTimeOriginal))
-                let newPath = timestamp.format("YYYY/MM/DD/") + parsedPath.base
-                Key = `${newPath}`
+                const timestamp = moment(exifDateParser.parse(data.image.DateTimeOriginal));
+                const newPath = timestamp.format('YYYY/MM/DD/') + parsedPath.base;
+                Key = `${newPath}`;
             } else {
-                let timestamp = moment(exifDateParser.parse(data.image.DateTimeOriginal))
-                console.log(`Could not parse out DateTimeOriginal in EXIF data. Timestamp would have been ${timestamp}`)
-                return callback()
+                const timestamp = moment(exifDateParser.parse(data.image.DateTimeOriginal));
+                console.log(`Could not parse out DateTimeOriginal in EXIF data. Timestamp would have been ${timestamp}`);
+                return callback();
             }
-            return callback()
+            return callback();
         }
 
-        let rate = (index + 1 - numIgnored)/(Date.now() - s3startTime)
+        const rate = (index + 1 - numIgnored) / (Date.now() - s3startTime); // eslint-disable-line no-mixed-operators, max-len
 
-        let params = {
-          localFile: resolvedPath,
-          s3Params: {
-            Bucket: BUCKET_NAME,
-            Key,
-            // other options supported by putObject, except Body and ContentLength.
-            // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
-          },
-        }
+        const params = {
+            localFile: resolvedPath,
+            s3Params: {
+                Bucket: BUCKET_NAME,
+                Key,
+                // other options supported by putObject, except Body and ContentLength.
+                // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
+            },
+        };
 
-        let uploader = client.uploadFile(params)
-        uploader.on('error', (err) => console.error('Upload error!', err.stack))
+        const uploader = client.uploadFile(params);
+        uploader.on('error', err => console.error('Upload error!', err.stack));
         // uploader.on('progress', () => console.log(`progress: ${(100 * uploader.progressAmount/uploader.progressTotal).toFixed(2)}%`, uploader.progressMd5Amount, uploader.progressAmount, uploader.progressTotal))
         uploader.on('end', () => {
-            let elapsed = (Date.now() - s3startTime)/60000
-            let remaining = ((total - index - 1)/rate)/60000
+            const elapsed = (Date.now() - s3startTime) / 60000;
+            const remaining = ((total - index - 1) / rate) / 60000;
             // console.log(`Finished uploading image ${index+1}/${total} ${resolvedPath} to S3`)
-            fs.writeFileSync(path.resolve(UPLOAD_LIST), resolvedPath + '\n', { flag: 'a' })
-            console.log(`[${index+1}/${total} ${elapsed.toFixed(2)} mins elapsed, ${remaining.toFixed(2)} remaining] ${resolvedPath} => ${Key}`)
-            return callback()
-        })
-    })
+            fs.writeFileSync(path.resolve(UPLOAD_LIST), `${resolvedPath}\n`, { flag: 'a' });
+            console.log(`[${index + 1}/${total} ${elapsed.toFixed(2)} mins elapsed, ${remaining.toFixed(2)} remaining] ${resolvedPath} => ${Key}`);
+            return callback();
+        });
+    });
 }
 
-let results = walk(path.resolve(process.argv[2]), MAC_PHOTOS_LIBRARY_FILTER_FUNCTION)
-let seriesFunctions = results.map((image, index, results) => {
-    return uploadToS3.bind(this, image, index, results.length)
-})
+const results = walk(path.resolve(process.argv[2]), MAC_PHOTOS_LIBRARY_FILTER_FUNCTION);
+const seriesFunctions = results
+    .map((image, index, allPaths) => uploadToS3.bind(this, image, index, allPaths.length));
 
-async.series(seriesFunctions, () => console.log(`Done uploading. Total time ${((Date.now() - t0)/60000).toFixed(2)} minutes.`))
+async.series(seriesFunctions, () => console.log(`Done uploading. Total time ${((Date.now() - t0) / 60000).toFixed(2)} minutes.`));
