d08dbf8600f7c4f5a0e4055238eb01e485c57c4a
ahw <andrewhallagan@gmail.com>
2016-10-22 13:59:03 -0400

    Tweak defaultFilterFunction

--- filter-functions.js
+++ filter-functions.js
@@ -1,5 +1,5 @@
-const IMAGE_FILENAME_REGEX = /(jpg|jpeg|gif|png)/i;
+const IMAGE_FILENAME_REGEX = /(jpg|jpeg|gif|png)$/i;
 
-module.exports.defaultFilterFunction = function (path) {
-    return IMAGE_FILENAME_REGEX.test(path);
+module.exports.defaultFilterFunction = function (fullPath) {
+    return IMAGE_FILENAME_REGEX.test(fullPath);
 }
